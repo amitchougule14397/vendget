b98f68199c37b4aba5f012a52134400b080325b3
amitchougule14397 <amitchougule14397@gmail.com>
2025-08-08 11:16:03 +0530

    new

---
+++ src/test/java/demo/demo_6.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demo_6 
+{
+	@Test
+    public void test3()
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ola.com/");
+        driver.quit();
+}
