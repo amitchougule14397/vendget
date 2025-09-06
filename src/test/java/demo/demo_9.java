bdf3ee9773d4aff941886a7a7ba31b92d1657999
amitchougule14397 <amitchougule14397@gmail.com>
2025-09-06 11:14:01 +0530

    adding new file

---
+++ src/test/java/demo/demo_9.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class demo_9
+{
+	@Test
+    public void test3()
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
+        driver.quit();
+    }
+}
